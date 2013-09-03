0aef5db611824b9c65336e9698507d3356a60a6c
Priit Kallas <kallaspriit@gmail.com>
2013-09-03 22:37:39 +0300

    Test

--- include/Blobber.h
+++ include/Blobber.h
@@ -87,7 +87,8 @@ class Blobber {
             Rgb() : red(0), green(0), blue(0) {}
             Rgb(char red, char green, char blue) : red(red), green(green), blue(blue) {}
 
-            unsigned char red, green, blue;
+            //unsigned char red, green, blue;
+            unsigned char blue, green, red;
         };
 
         struct Blob {
