fa15be0c02dadf5bc212700e83393ed38e25a0c0
Priit Kallas <kallaspriit@gmail.com>
2013-09-04 23:08:32 +0300

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -103,7 +103,7 @@ int main(int argc, char* argv[]) {
 		processor1->convertRGB = processor2->convertRGB = debug;
 		processor1->renderBlobs = processor2->renderBlobs = debug;
 
-		//__int64 startTime = Util::timerStart();
+		__int64 startTime = Util::timerStart();
 
 		// camera1
 		if (camera1->isAcquisitioning()) {
@@ -154,8 +154,8 @@ int main(int argc, char* argv[]) {
 			gui->update();
 		}
 
-		//std::cout << "! Total time: " << Util::timerEnd(startTime) << ", " << fpsCounter->getFps() << "FPS" << std::endl << std::endl;
-		std::cout << "! " << fpsCounter->getFps() << "FPS" << std::endl;
+		std::cout << "! Total time: " << Util::timerEnd(startTime) << ", " << fpsCounter->getFps() << "FPS " << (gotFrame1 ? '+' : '-') << (gotFrame2 ? '+' : '-') << std::endl << std::endl;
+		//std::cout << "! " << fpsCounter->getFps() << "FPS " << (gotFrame1 ? '+' : '-') << (gotFrame2 ? '+' : '-') << std::endl;
 
 
 		fpsCounter->step();
