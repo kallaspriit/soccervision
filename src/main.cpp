e184298ed6d6bfcba2aae0d25bc887fac5dfea44
Priit Kallas <kallaspriit@gmail.com>
2013-09-09 23:52:03 +0300

    Updated

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,8 @@
 
 int main(int argc, char* argv[]) {
 	#ifdef _DEBUG
-	_CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+		_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+		_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
 	#endif
 
 	bool showGui = false;
