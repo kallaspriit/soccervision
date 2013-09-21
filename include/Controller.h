8ea28dcb7241532c1554685142cc657e295295ca
Priit Kallas <kallaspriit@gmail.com>
2013-09-21 22:00:51 +0300

    Updated

--- include/Controller.h
+++ include/Controller.h
@@ -11,19 +11,21 @@ class Robot;
 class Command;
 
 class Controller : public InfoBoardListener {
-    public:
-        Controller(Robot* robot) : robot(robot) {}
-		virtual void onEnter() {}
-		virtual void onExit() {}
-		virtual bool handleRequest(std::string request) { return false; }
-		virtual bool handleCommand(const Command& cmd) { return false; }
-		virtual void onGoRequestedChange(bool isGoRequested) {}
-		virtual void onTargetSideChange(Side newTargetSide) {}
-		virtual void step(float dt, Vision::Results* visionResults) = 0;
-		virtual std::string getJSON() { return "null"; }
 
-    protected:
-        Robot* robot;
+public:
+    Controller(Robot* robot) : robot(robot) {}
+	virtual void onEnter() {}
+	virtual void onExit() {}
+	virtual bool handleRequest(std::string request) { return false; }
+	virtual bool handleCommand(const Command& cmd) { return false; }
+	virtual void onGoRequestedChange(bool isGoRequested) {}
+	virtual void onTargetSideChange(Side newTargetSide) {}
+	virtual void step(float dt, Vision::Results* visionResults) = 0;
+	virtual std::string getJSON() { return "null"; }
+
+protected:
+    Robot* robot;
+
 };
 
 typedef std::map<std::string, Controller*> ControllerMap;
