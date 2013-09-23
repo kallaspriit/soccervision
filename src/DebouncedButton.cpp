abca5e24f2585e4e8c0145c5012fb616ebba7966
Priit Kallas <kallaspriit@gmail.com>
2013-09-23 20:09:29 +0300

    Updated

--- src/DebouncedButton.cpp
+++ src/DebouncedButton.cpp
@@ -1,9 +1,13 @@
 #include "DebouncedButton.h"
 #include "Util.h"
 
+#include <iostream>
+
 DebouncedButton::DebouncedButton(float period) : period(period), lastChangeTime(0) {}
 
 bool DebouncedButton::toggle() {
+	std::cout << Util::duration(lastChangeTime) << " vs " << period << ", toggle: " << (Util::duration(lastChangeTime) > (double)period ? "yes" : "no") << std::endl;
+
 	if (Util::duration(lastChangeTime) > (double)period) {
 		lastChangeTime = Util::millitime();
 
