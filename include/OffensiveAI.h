813348a127fecd4547a382934feeb2c209777610
Priit Kallas <kallaspriit@gmail.com>
2013-09-27 18:29:36 +0300

    Updated

--- include/OffensiveAI.h
+++ include/OffensiveAI.h
@@ -20,7 +20,7 @@ public:
 		virtual void onExit() {}
 		virtual void step(float dt, float totalDuration, float stateDuration) = 0;
 
-	private:
+	protected:
 		OffensiveAI* ai;
 
 	};
@@ -29,7 +29,19 @@ public:
 
 	public:
 		IdleState(OffensiveAI* ai) : State(ai) {}
-		void step(float dt, float totalDuration, float stateDuration) {}
+		void onEnter();
+		void onExit();
+		void step(float dt, float totalDuration, float stateDuration);
+
+	};
+
+	class FindBallState : public State {
+
+	public:
+		FindBallState(OffensiveAI* ai) : State(ai) {}
+		void onEnter();
+		void onExit();
+		void step(float dt, float totalDuration, float stateDuration);
 
 	};
 
@@ -44,6 +56,8 @@ public:
 	void setState(std::string state);
     bool handleRequest(std::string request);
     bool handleCommand(const Command& cmd);
+	void handleToggleSideCommand();
+	void handleToggleGoCommand();
 	void handleCommunicationMessage(std::string message);
     void step(float dt, Vision::Results* visionResults);
 	void reset();
@@ -58,6 +72,7 @@ private:
 	DebouncedButton toggleGoBtn;
 	States states;
 	State* currentState;
+	std::string currentStateName;
 	bool running;
 	float totalDuration;
 	float currentStateDuration;
--- src/OffensiveAI.cpp
+++ src/OffensiveAI.cpp
@@ -28,10 +28,14 @@ void OffensiveAI::reset() {
 }
 
 void OffensiveAI::onEnter() {
+	std::cout << "! Now using offensive AI algorithm" << std::endl;
+
 	reset();
 }
 
 void OffensiveAI::onExit() {
+	std::cout << "! Stopping offensive AI algorithm" << std::endl;
+
 	reset();
 }
 
@@ -53,6 +57,7 @@ void OffensiveAI::setState(std::string state) {
 	newState->onEnter();
 
 	currentState = newState;
+	currentStateName = state;
 }
 
 bool OffensiveAI::handleRequest(std::string request) {
@@ -60,7 +65,43 @@ bool OffensiveAI::handleRequest(std::string request) {
 }
 
 bool OffensiveAI::handleCommand(const Command& cmd) {
-	return false;
+	if (cmd.name == "toggle-side") {
+        handleToggleSideCommand();
+    } else if (cmd.name == "toggle-go") {
+        handleToggleGoCommand();
+    } else {
+		return false;
+	}
+
+	return true;
+}
+
+void OffensiveAI::handleToggleSideCommand() {
+	if (!toggleSideBtn.toggle()) {
+		return;
+	}
+
+	if (targetSide == Side::BLUE) {
+		targetSide = Side::YELLOW;
+	} else {
+		targetSide = Side::BLUE;
+	}
+
+	com->send("target:" + Util::toString(targetSide));
+}
+
+void OffensiveAI::handleToggleGoCommand() {
+	if (!toggleGoBtn.toggle()) {
+		return;
+	}
+
+	running = !running;
+
+	if (running) {
+		setState("find-ball");
+	} else {
+		setState("idle");
+	}
 }
 
 void OffensiveAI::handleCommunicationMessage(std::string message) {
@@ -78,10 +119,50 @@ void OffensiveAI::step(float dt, Vision::Results* visionResults) {
 
 void OffensiveAI::setupStates() {
 	states["idle"] = new IdleState(this);
+	states["find-ball"] = new FindBallState(this);
 
 	setState("idle");
 }
 
 std::string OffensiveAI::getJSON() {
-	return "null";
+	std::stringstream stream;
+
+	stream << "{";
+	stream << "\"state\": \"" << currentStateName << "\",";
+	stream << "\"targetSide\": " << targetSide << ",";
+	stream << "\"running\": " << (running ? "true" : "false") << ",";
+	stream << "\"stateDuration\": \"" << currentStateDuration << "\",";
+	stream << "\"totalDuration\": \"" << totalDuration << "\"";
+	stream << "}";
+
+	return stream.str();
+}
+
+// idle
+void OffensiveAI::IdleState::onEnter() {
+	std::cout << "! Enter idle state" << std::endl;
+}
+
+void OffensiveAI::IdleState::onExit() {
+	std::cout << "! Exit idle state" << std::endl;
+}
+
+void OffensiveAI::IdleState::step(float dt, float totalDuration, float stateDuration) {
+	std::cout << "! Step idle state: " << dt << ", " << totalDuration << ", " << stateDuration << std::endl;
+
+	ai->setState("find-ball");
+}
+
+
+// find ball
+void OffensiveAI::FindBallState::onEnter() {
+	std::cout << "! Enter find ball state" << std::endl;
+}
+
+void OffensiveAI::FindBallState::onExit() {
+	std::cout << "! Exit find ball state" << std::endl;
+}
+
+void OffensiveAI::FindBallState::step(float dt, float totalDuration, float stateDuration) {
+	std::cout << "! Step find ball state: " << dt << ", " << totalDuration << ", " << stateDuration << std::endl;
 }
