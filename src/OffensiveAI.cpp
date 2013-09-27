9cde3f48c848a67095905896d603da5fe5a9fc1e
Priit Kallas <kallaspriit@gmail.com>
2013-09-27 18:50:32 +0300

    Updated

--- src/OffensiveAI.cpp
+++ src/OffensiveAI.cpp
@@ -69,10 +69,14 @@ void OffensiveAI::setState(std::string state) {
 }
 
 bool OffensiveAI::handleRequest(std::string request) {
+	std::cout << "! Offensive AI request: " << request << std::endl;
+
 	return false;
 }
 
 bool OffensiveAI::handleCommand(const Command& cmd) {
+	std::cout << "! Offensive AI command: " << cmd.name << " " << Util::toString(cmd.parameters) << std::endl;
+
 	if (cmd.name == "toggle-side") {
         handleToggleSideCommand();
     } else if (cmd.name == "toggle-go") {
