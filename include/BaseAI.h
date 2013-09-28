80efc939317d82f7140ff0f40b5f1d97b4665bad
Priit Kallas <kallaspriit@gmail.com>
2013-09-28 15:44:03 +0300

    Added BaseAI

---
+++ include/BaseAI.h
@@ -0,0 +1,45 @@
+#ifndef BASEAI_H
+#define BASEAI_H
+
+#include "Controller.h"
+#include "Vision.h"
+#include "DebouncedButton.h"
+#include "Config.h"
+
+#include <string>
+#include <map>
+
+class BaseAI : public Controller {
+
+public:
+	class State {
+
+	public:
+		State(BaseAI* ai) : ai(ai) {}
+		virtual void onEnter() {}
+		virtual void onExit() {}
+		virtual void step(float dt, float totalDuration, float stateDuration) = 0;
+
+	protected:
+		BaseAI* ai;
+
+	};
+
+	typedef std::map<std::string, State*> States;
+	typedef States::iterator StatesIt;
+
+	BaseAI(Robot* robot, Communication* com);
+	virtual ~BaseAI();
+	virtual void setState(std::string state);
+	virtual void handleCommunicationMessage(std::string message);
+
+protected:
+	States states;
+	State* currentState;
+	std::string currentStateName;
+	float totalDuration;
+	float currentStateDuration;
+
+};
+
+#endif // BASEAI_H
---
+++ src/BaseAI.cpp
@@ -0,0 +1,46 @@
+#include "BaseAI.h"
+
+BaseAI::BaseAI(Robot* robot, Communication* com) : Controller(robot, com), currentState(NULL), currentStateName(""), totalDuration(0.0f), currentStateDuration(0.0f) {
+
+}
+
+BaseAI::~BaseAI() {
+	for (StatesIt it = states.begin(); it != states.end(); it++) {
+		delete it->second;
+	}
+
+	states.clear();
+}
+
+void BaseAI::setState(std::string state) {
+	if (states.find(state) == states.end()) {
+		std::cout << "- Invalid state '" << state << "' requested" << std::endl;
+
+		return;
+	}
+
+	State* newState = states[state];
+
+	if (currentState != NULL) {
+		std::cout << "! Switched state from " << currentStateName << " to " << state << std::endl;
+
+		currentState->onExit();
+	} else {
+		std::cout << "! Set initial AI state to " << state << std::endl;
+	}
+
+	currentStateDuration = 0.0f;
+
+	newState->onEnter();
+
+	currentState = newState;
+	currentStateName = state;
+}
+
+void BaseAI::handleCommunicationMessage(std::string message) {
+	if (Command::isValid(message)) {
+        Command command = Command::parse(message);
+
+		handleCommand(command);
+	}
+}
