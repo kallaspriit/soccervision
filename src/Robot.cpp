978b72192cfad77676a17f52452b66e81e0c97fc
Priit Kallas <kallaspriit@gmail.com>
2013-09-29 15:00:53 +0300

    Updated

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -121,11 +121,11 @@ void Robot::step(float dt, Vision::Results* visionResults) {
 
 	// send target speeds
 	com->send("speeds:"
-		+ Util::toString(wheelRR->getTargetSpeed()) + ":"
-		+ Util::toString(wheelFR->getTargetSpeed()) + ":"
-		+ Util::toString(dribbler->getTargetSpeed()) + ":"
 		+ Util::toString(wheelFL->getTargetSpeed()) + ":"
-		+ Util::toString(wheelRL->getTargetSpeed())
+		+ Util::toString(wheelFR->getTargetSpeed()) + ":"
+		+ Util::toString(wheelRL->getTargetSpeed()) + ":"
+		+ Util::toString(wheelRR->getTargetSpeed()) + ":"
+		+ Util::toString(dribbler->getTargetSpeed())
 	);
 
 	movement = odometer->calculateMovement(
