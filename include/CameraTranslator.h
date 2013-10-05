0e24cff86d6982d9737594ad6646886066113d64
Priit Kallas <kallaspriit@gmail.com>
2013-10-05 20:23:56 +0300

    Updated

--- include/CameraTranslator.h
+++ include/CameraTranslator.h
@@ -1,16 +1,18 @@
 #ifndef CAMERATRANSLATOR_H
 #define CAMERATRANSLATOR_H
 
+#include <math.h>
+
 class CameraTranslator {
 
 public:
 	struct WorldPosition {
 		WorldPosition(float dx, float dy, float distance, float angle) : dx(dx), dy(dy), distance(distance), angle(angle) {}
 
-		float dx; // in meters
+		float dx;
 		float dy;
 		float distance;
-		float angle; // in radians
+		float angle;
 	};
 
 	struct CameraPosition {
@@ -20,21 +22,26 @@ public:
 		int y;
 	};
 
-	void setConstants(float A, float B, float C, float horizon) {
-		this->A = A;
-		this->B = B;
-		this->C = C;
-		this->horizon = horizon;
-	};
+	void setConstants(
+		float A, float B, float C,
+		float k1, float k2, float k3,
+		float horizon, int cameraWidth, int cameraHeight);
 
 	WorldPosition getWorldPosition(int cameraX, int cameraY);
 	CameraPosition getCameraPosition(float dx, float dy);
+	CameraTranslator::CameraPosition CameraTranslator::undistort(int x, int y);
+	CameraTranslator::CameraPosition CameraTranslator::distort(int x, int y);
 
 private:
 	float A;
 	float B;
 	float C;
+	float k1;
+	float k2;
+	float k3;
 	float horizon;
+	int cameraWidth;
+	int cameraHeight;
 
 };
 
