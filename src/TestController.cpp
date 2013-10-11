0bfcc64168d8b8d538c4287effb1db819613edc3
Priit Kallas <kallaspriit@gmail.com>
2013-10-12 01:12:28 +0300

    Updated

--- src/TestController.cpp
+++ src/TestController.cpp
@@ -239,9 +239,8 @@ void TestController::FetchBallInfrontState::step(float dt, Vision::Results* visi
 	float dribblerStartDistance = 0.5f;
 	//int maxSideSpeedThreshold = 25; // side speed is maximal at this distance from side
 	//int minSideSpeedThreshold = Config::cameraWidth / 4; // side speed is canceled starting from this distance from side
-	//int maxSideSpeedThreshold = 0; // side speed is maximal at this distance from side
-	int maxSideSpeedThreshold = 100; // side speed is maximal at this distance from side
-	int minSideSpeedThreshold = Config::cameraWidth / 2; // side speed is canceled starting from this distance from side
+	int maxSideSpeedThreshold = 0; // side speed is maximal at this distance from side
+	int minSideSpeedThreshold = Config::cameraWidth / 4; // side speed is canceled starting from this distance from side
 	float sideSpeed, forwardSpeed;
 
 	if (ai->parameters[0].length() > 0) approachSpeed = Util::toFloat(ai->parameters[0]);
