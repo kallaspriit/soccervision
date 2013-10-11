04059161e19c8913d936274d6a4ca742cfc3ecfc
Priit Kallas <kallaspriit@gmail.com>
2013-10-11 23:46:41 +0300

    Updated

--- src/Coilgun.cpp
+++ src/Coilgun.cpp
@@ -1,10 +1,11 @@
 #include "Coilgun.h"
+#include "Communication.h"
 #include "Util.h"
 #include "Config.h"
 
 #include <iostream>
 
-Coilgun::Coilgun() : kickStrength(0), doKick(false), doDischarge(false), charging(false), lastKickTime(0) {
+Coilgun::Coilgun(Communication* com) : com(com), lastKickTime(0) {
 
 };
 
@@ -13,62 +14,23 @@ Coilgun::~Coilgun() {
 }
 
 void Coilgun::charge() {
-	/*if (!serial->isOpen()) {
-		return;
-	}
-
-	serial->writeln("c");*/
-
-	charging = true;
+	com->send("charge");
 }
 
 void Coilgun::discharge() {
-	/*if (!serial->isOpen()) {
-		return;
-	}
-
-	serial->writeln("d");*/
-
-	charging = false;
+	com->send("discharge");
 }
 
-void Coilgun::kick(int strength) {
-	kickStrength = strength;
-	doKick = true;
+void Coilgun::kick(int microseconds) {
+	if (lastKickTime == 0.0 || Util::duration(lastKickTime) >= Config::minKickInterval) {
+		std::cout << "! Kicking: " << microseconds << std::endl;
 
-	std::cout << "! Kick requested: " << kickStrength << std::endl;
+		com->send("kick:" + Util::toString(microseconds));
+	} else {
+		std::cout << "! Another kick requested too soon" << std::endl;
+	}
 }
 
 void Coilgun::step(float dt) {
-	/*if (!serial->isOpen()) {
-		return;
-	}
-
-	serial->writeln("p"); // send ping to prevent discharge
-
-	if (doDischarge) {
-		serial->writeln("d");
-
-		doDischarge = false;
-	} else if (doKick) {
-		doKick = false;
-
-		if (!charging) {
-			std::cout << "- Kick requested but autocharging not active" << std::endl;
-		}
-
-		double currentTime = Util::millitime();
-
-		if (currentTime - lastKickTime < Config::kickBackoffTime) {
-			std::cout << "- Another kick requested too soon" << std::endl;
-
-			return;
-		}
-
-		std::cout << "! KICK: " << kickStrength << std::endl;
-
-		serial->writeln("k" + Util::toString(kickStrength));
-
-		lastKickTime = currentTime;
-	}*/
+	
 }
