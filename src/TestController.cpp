02534ac082eb1c87c7d8e2e72e12d34cf3c898a1
Priit Kallas <kallaspriit@gmail.com>
2013-10-11 20:53:39 +0300

    Updated

--- src/TestController.cpp
+++ src/TestController.cpp
@@ -238,7 +238,7 @@ void TestController::FetchBallInfrontState::step(float dt, Vision::Results* visi
 		minSideSpeedThreshold = 250;
 	} else {
 		minSideSpeedThreshold = Config::cameraWidth / 2;
-		maxSideSpeedThreshold = minSideSpeedThreshold - 100;
+		maxSideSpeedThreshold = Config::cameraWidth / 4;
 	}
 
 	if (ai->parameters[0].length() > 0) sideP = Util::toFloat(ai->parameters[0]);
