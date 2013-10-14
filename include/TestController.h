a817353961dcd7f17716dba0d4717c402de445de
Priit Kallas <kallaspriit@gmail.com>
2013-10-14 19:56:48 +0300

    Updated

--- include/TestController.h
+++ include/TestController.h
@@ -87,12 +87,11 @@ public:
 	class FetchBallStraightState : public State {
 
 	public:
-		FetchBallStraightState(TestController* ai) : State(ai), braking(false) {}
+		FetchBallStraightState(TestController* ai) : State(ai) {}
 		void step(float dt, Vision::Results* visionResults, Robot* robot, float totalDuration, float stateDuration);
 
 	private:
 		float getTargetAngle(float goalX, float goalY, float ballX, float ballY, float D);
-		bool braking;
 
 	};
 
