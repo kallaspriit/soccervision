b414ea4bfc2dc0d388b2622a5bcf6a7ff4ef2bfa
Priit Kallas <kallaspriit@gmail.com>
2013-10-15 21:43:49 +0300

    Updated

--- src/TestController.cpp
+++ src/TestController.cpp
@@ -397,18 +397,16 @@ void TestController::FetchBallBehindState::step(float dt, Vision::Results* visio
 	if (ai->parameters[1].length() > 0) approachP = Util::toFloat(ai->parameters[1]);
 
 	//float targetAngle = ai->getTargetAngle(goal->distanceX, goal->distanceY * (goal->behind ? -1.0f : 1.0f), ball->distanceX, ball->distanceY * (ball->behind ? -1.0f : 1.0f), offsetDistance, TargetMode::RIGHT);
-	float targetAngle = ai->getTargetAngle(goal->distanceX, goal->distanceY * (goal->behind ? -1.0f : 1.0f), ball->distanceX, ball->distanceY * (ball->behind ? -1.0f : 1.0f), offsetDistance, TargetMode::INLINE);
+	float targetAngle = ai->getTargetAngle(goal->distanceX * (goal->behind ? -1.0f : 1.0f), goal->distanceY * (goal->behind ? -1.0f : 1.0f), ball->distanceX * (ball->behind ? -1.0f : 1.0f), ball->distanceY * (ball->behind ? -1.0f : 1.0f), offsetDistance, TargetMode::INLINE);
 	float approachSpeed = approachP * Math::map(stateDuration, 0.0f, startAccelerationDuration, 0.0f, 1.0f);
 
 	ai->dbg("offsetDistance", offsetDistance);
 	ai->dbg("approachSpeed", approachSpeed);
 	ai->dbg("targetAngle", Math::radToDeg(targetAngle));
-	ai->dbg("goal->distanceX", goal->distanceX);
-	ai->dbg("goal->distanceY1", goal->distanceY);
-	ai->dbg("goal->distanceY2", goal->distanceY * (goal->behind ? -1.0f : 1.0f));
-	ai->dbg("ball->distanceX", ball->distanceX);
-	ai->dbg("ball->distanceY1", ball->distanceY);
-	ai->dbg("ball->distanceY2", ball->distanceY * (ball->behind ? -1.0f : 1.0f));
+	ai->dbg("goal->distanceX", goal->distanceX * (goal->behind ? -1.0f : 1.0f));
+	ai->dbg("goal->distanceY", goal->distanceY * (goal->behind ? -1.0f : 1.0f));
+	ai->dbg("ball->distanceX", ball->distanceX * (ball->behind ? -1.0f : 1.0f));
+	ai->dbg("ball->distanceY", ball->distanceY * (ball->behind ? -1.0f : 1.0f));
 
 	robot->setTargetDir(Math::Rad(targetAngle), approachSpeed);
 	robot->lookAt(goal);
