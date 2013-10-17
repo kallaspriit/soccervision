b5178d021a92ac882bec0758f8c1c4385edf297d
Priit Kallas <kallaspriit@gmail.com>
2013-10-17 19:49:59 +0300

    Updated

--- src/Dribbler.cpp
+++ src/Dribbler.cpp
@@ -52,7 +52,7 @@ bool Dribbler::handleCommand(const Command& cmd) {
 			ballDetected = false;
 		}
 
-		//std::cout << "@ BALL DETECTED: " << ballDetected << std::endl;
+		std::cout << "@ BALL DETECTED: " << ballDetected << std::endl;
 
 		return true;
 	}
