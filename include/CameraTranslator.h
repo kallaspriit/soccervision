bfad37bf61d2e95b03c32064e992125b7f7c2e6b
Mark Laane <marklane+Git@gmail.com>
2013-10-20 00:46:37 +0300

    Undistort & distort - Mapping introduced
    *bit buggy

--- include/CameraTranslator.h
+++ include/CameraTranslator.h
@@ -2,10 +2,20 @@
 #define CAMERATRANSLATOR_H
 
 #include <math.h>
+#include "Maths.h"
+
+#include <string>
+#include <vector>
+
+#include <iostream> //cout
+#include <fstream>
+#include <sstream>
 
 class CameraTranslator {
-
 public:
+	typedef std::vector <int> cameraMapRow;
+	typedef std::vector <cameraMapRow> cameraMap;
+
 	struct WorldPosition {
 		WorldPosition() : dx(0.0f), dy(0.0f), distance(0.0f), angle(0.0f) {}
 		WorldPosition(float dx, float dy, float distance, float angle) : dx(dx), dy(dy), distance(distance), angle(angle) {}
@@ -26,28 +36,22 @@ public:
 
 	CameraTranslator() : A(0.0f), B(0.0f), C(0.0f), k1(0.0f), k2(0.0f), k3(0.0f), horizon(0.0f), cameraWidth(0), cameraHeight(0) {}
 
-
-	/* Esimese Kaamera konstandid testimiseks:
-		A=-65
-		B=252000
-		C=187
-		Horizon=3.0
-
-		float distortionFocus = 1230.9756548915236;
-		float k1 = -1.7578689303905209e-001;
-		float k2 = -2.5481256902241417e-001;
-		float k3 =  7.6993101864245295e-001;
-	*/
-
 	void setConstants(
 		int cameraWidth, int cameraHeight,
 		float A, float B, float C, float horizon, 
 		float distortionFocus,
 		float k1, float k2, float k3);
 
+	bool loadMapping(char* xFile, char* yFile);
+	void CameraTranslator::printMapping();
+
+
 	WorldPosition getWorldPosition(int cameraX, int cameraY);
 	CameraPosition getCameraPosition(float dx, float dy);
+
+	CameraTranslator::CameraPosition CameraTranslator::distort(int x, int y);
 	CameraTranslator::CameraPosition CameraTranslator::undistort(int x, int y);
+	
 
 private:
 	int cameraWidth;
@@ -64,6 +68,13 @@ private:
 	float k1;
 	float k2;
 	float k3;
+
+	cameraMap xMap;
+	cameraMap yMap;
+	void CameraTranslator::printMap(cameraMap& map);
+
+	//Overload for loading cameraMap from file
+	friend std::istream& operator >> ( std::istream& inputStream, cameraMap& map);
 };
 
 #endif
--- src/CameraTranslator.cpp
+++ src/CameraTranslator.cpp
@@ -1,11 +1,7 @@
 #include "CameraTranslator.h"
-#include "Maths.h"
-
-#include <iostream>
 
 CameraTranslator::WorldPosition CameraTranslator::getWorldPosition(int cameraX, int cameraY) {
-	//CameraTranslator::CameraPosition undistorted = CameraTranslator::undistort(cameraX, cameraY);
-	CameraTranslator::CameraPosition undistorted = CameraTranslator::CameraPosition(cameraX, cameraY);
+	CameraTranslator::CameraPosition undistorted = CameraTranslator::undistort(cameraX, cameraY);
 
 	float pixelVerticalCoord = undistorted.y - this->horizon;
 	int pixelRight = undistorted.x - this->cameraWidth / 2;
@@ -26,8 +22,7 @@ CameraTranslator::CameraPosition CameraTranslator::getCameraPosition(float world
 	float cameraY = pixelVerticalCoord + this->horizon;
 	float cameraX = pixelRight + this->cameraWidth / 2;
 
-	//return CameraTranslator::distort((int)Math::round(cameraX, 0), (int)Math::round(cameraY, 0));
-	return CameraTranslator::CameraPosition((int)Math::round(cameraX, 0), (int)Math::round(cameraY, 0));
+	return CameraTranslator::distort((int)Math::round(cameraX, 0), (int)Math::round(cameraY, 0));
 }
 
 void CameraTranslator::setConstants(
@@ -49,27 +44,107 @@ void CameraTranslator::setConstants(
 }
 
 CameraTranslator::CameraPosition CameraTranslator::undistort(int distortedX, int distortedY) {
-	/*
-	//Conversion for undistorting  (normalization?)
-	float x = (float(distortedX) - cameraWidth / 2.0 ) / distortionFocus;
-	float y = (float(distortedY) - cameraHeight / 2.0) / distortionFocus;
-	//Undistort
-	float r2 = x*x + y*y; // distance squared
-	float multipler = 1 + 
-		k1 * 1 * r2 +
-		k2 * 2 * r2 + 
-		k3 * 3 * r2;
-	x *= multipler;
-	y *= multipler;
-	//Convert back
-	float undistortedX = x * distortionFocus + cameraWidth / 2.0;
-	float undistortedY = y * distortionFocus + cameraHeight / 2.0;
+	int undistortedX = xMap[distortedY][distortedX];
+	int undistortedY = yMap[distortedY][distortedX];
 
 	return CameraPosition(
 		int(undistortedX),
 		int(undistortedY)
 	);
-	*/
-	return CameraPosition(0,0);
+}
+CameraTranslator::CameraPosition CameraTranslator::distort(int undistortedX, int undistortedY) {
+	// Conversion for distorting  (normalization?)
+	float x = (float(undistortedX) - cameraWidth / 2.0 ) / distortionFocus;
+	float y = (float(undistortedY) - cameraHeight / 2.0) / distortionFocus;
+	// distort
+	float r2 = x*x + y*y; // distance squared
+	float multipler = 1 + 
+		k1 *  r2 +
+		k2 *  r2 * r2 + 
+		k3 *  r2 * r2 * r2;
+	x *= multipler;
+	y *= multipler;
+	//Convert back
+	float distortedX = x * distortionFocus + cameraWidth / 2.0;
+	float distortedY = y * distortionFocus + cameraHeight / 2.0;
+
+	return CameraPosition(
+		int(distortedX),
+		int(distortedY)
+	);
+}
+
+bool CameraTranslator::loadMapping(char* xFileName, char* yFileName){
+	std::ifstream fileStream;
+
+	// Load xMap
+	fileStream.open(xFileName);
+	if(fileStream.is_open()){
+		fileStream >> xMap;
+		if (!fileStream.eof()) {
+			std::cout << "failed to load xMap" << std::endl;
+			return false;
+		}
+	}
+	else{
+		std::cout << "failed to open xMap file" << std::endl;
+	}
+
+	// Load yMap
+	fileStream.close();
+	fileStream.open(yFileName);
+	if(fileStream.is_open()){
+		fileStream >> yMap;
+		if (!fileStream.eof()) {
+			std::cout << "failed to load yMap" << std::endl;
+			return false;
+		}
+	}
+	else{
+		std::cout << "failed to open yMap file" << std::endl;
+	}
+	fileStream.close();
+
+	return true;
+}
+
+void CameraTranslator::printMapping(){
+	printf("0:0 = %i\n",xMap[0][0]);
+	std::cout << "X-Map:" << std::endl;
+	printMap(xMap);
+	std::cout << "Y-Map:" << std::endl;
+	printMap(yMap);
+}
+void CameraTranslator::printMap(cameraMap& map){
+	for (cameraMap::const_iterator row = map.begin(); row != map.end(); ++row) {
+		for (cameraMapRow::const_iterator col = row->begin(); col != row->end(); ++col) {
+			printf("%4i,",*col);
+		}
+		std::cout << std::endl;
+	}
+}
+
+std::istream& operator >> ( std::istream& inputStream, CameraTranslator::cameraMap& map) {
+	map.clear();
+	CameraTranslator::cameraMapRow mapRow;
+
+	//Get a line from input stream..
+	std::string lineString;
+	while(getline(inputStream, lineString)) {
+		mapRow.clear();
+		std::stringstream lineStream(lineString);
+		//Get a field from a line stream
+		std::string fieldString;
+		while (getline(lineStream, fieldString, ',')) {
+			std::stringstream fieldStream(fieldString);
+			//extract the field as an integer
+			int field;
+			fieldStream >> field;
+			mapRow.push_back(field);
+		}
+		map.push_back(mapRow);
+	}
+
+	return inputStream;  
 }
 
