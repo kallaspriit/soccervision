169655aa6126e6a62f905b436882d6e82c06edbe
Priit Kallas <kallaspriit@gmail.com>
2013-10-20 18:14:53 +0300

    Updated

--- src/Vision.cpp
+++ src/Vision.cpp
@@ -907,7 +907,7 @@ Vision::PathMetric Vision::getPathMetric(int x1, int y1, int x2, int y2, std::ve
 					if (debug) {
 						canvas.drawMarker(x, y, 0, 128, 0);
 					}
-				} else if ((sawWhite || firstColor == "white") && previousBlack >= 2) {
+				} else if ((sawWhite || firstColor == "white") && previousBlack >= 1) {
 					crossingGreenWhiteBlackGreen = true;
 
 					if (debug) {
