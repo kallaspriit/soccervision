bd7fd8ebd5e92d18af5852e89d694afc809f4716
Priit Kallas <kallaspriit@gmail.com>
2013-11-17 15:53:15 +0200

    Updated

--- src/SoccerBot.cpp
+++ src/SoccerBot.cpp
@@ -514,8 +514,8 @@ void SoccerBot::setupControllers() {
 }
 
 void SoccerBot::setupXimeaCamera(std::string name, XimeaCamera* camera) {
-	//camera->setGain(6);
-	camera->setGain(4);
+	camera->setGain(6);
+	//camera->setGain(4);
 	camera->setExposure(10000);
 	camera->setFormat(XI_RAW8);
 	camera->setAutoWhiteBalance(false);
