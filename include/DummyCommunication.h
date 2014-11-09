6f22451e72f305e0a12f93b3ede3b580e327bc81
Priit Kallas <kallaspriit@gmail.com>
2014-11-09 16:54:16 +0200

    Added DummyCommunication.h

---
+++ include/DummyCommunication.h
@@ -0,0 +1,30 @@
+#ifndef DUMMY_COMMUNICATION_H
+#define DUMMY_COMMUNICATION_H
+
+#include "AbstractCommunication.h"
+
+#include <iostream>
+
+class DummyCommunication : public AbstractCommunication {
+
+public:
+
+	DummyCommunication() {};
+
+	void send(std::string message) {
+		std::cout << "SEND: " << message << std::endl;
+	};
+
+	bool gotMessages() {
+		return false;
+	};
+
+	std::string dequeueMessage() {
+		return "";
+	};
+
+	void close() {};
+
+};
+
+#endif // DUMMY_COMMUNICATION_H
