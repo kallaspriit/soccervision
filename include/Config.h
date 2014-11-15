f0cc52022bb2b4104166183afb0c6322ffc55e6a
Priit Kallas <kallaspriit@gmail.com>
2014-11-15 20:39:43 +0200

    Updated

--- include/Config.h
+++ include/Config.h
@@ -187,7 +187,7 @@ namespace Config {
 	// how much should the centerline be inside the goal to kick ball
 	// 0.5 means that the camera centerline needs to be in the center 50% of the goal, 0.75 in the 75% so smaller means more accurate but takes longer
 	//const float goalKickThreshold = 0.25f;
-	const float goalKickThreshold = 0.1f;
+	const float goalKickThreshold = 0.75f;
 	//const int goalKickValidFrames = 4;
 	const int goalKickValidFrames = 1;
 
