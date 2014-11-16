18df68eada983e74ae74e487712ffd6de3888493
Priit Kallas <kallaspriit@gmail.com>
2014-11-16 04:20:11 +0200

    Updated

--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -392,14 +392,14 @@ void Canvas::drawText(int imageX, int imageY, std::string text, int red, int gre
         return;
     }
 
-	int paddingHor = 4;
+	int paddingHor = 3;
 	int paddingVer = 2;
 	int charWidth = 9;
 	int charHeight = 7;
 
     const char* str = text.c_str();
 
-	fillBox(imageX - paddingHor, imageY - paddingVer, text.length() * charWidth + paddingHor * 2, charHeight + paddingVer * 2, 255, 255, 255);
+	fillBox(imageX - paddingHor - 1, imageY - paddingVer + 1, text.length() * charWidth + paddingHor * 2, charHeight + paddingVer * 2, 255, 255, 255);
 
     for (unsigned int i = 0; i < text.size(); i++) {
 		drawChar(imageX + i * charWidth, imageY, str[i], red, green, blue);
