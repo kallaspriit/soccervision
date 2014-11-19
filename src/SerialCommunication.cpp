43ac85020de7975f627b0889073b2a88d87f01e9
Priit Kallas <kallaspriit@gmail.com>
2014-11-19 14:54:02 +0200

    Updated

--- src/SerialCommunication.cpp
+++ src/SerialCommunication.cpp
@@ -117,8 +117,9 @@ std::string SerialCommunication::dequeueMessage() {
 
 	messages.pop();
 
-	if (message.substr(0, 7) != "<speeds" && message.substr(0, 4) != "<adc") {
-		std::cout << "R " << message << " [" << messageCount << "]" << std::endl;
+	//if (message.substr(0, 7) != "<speeds" && message.substr(0, 4) != "<adc") {
+	if (message.substr(0, 1) != "<") {
+		std::cout << "R '" << message << "' [" << messageCount << "]" << std::endl;
 	}
 
 	return message;
