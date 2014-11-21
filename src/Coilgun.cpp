5902dee54e817d4a045abc2c29a61c58be92d128
Priit Kallas <kallaspriit@gmail.com>
2014-11-21 19:35:23 +0200

    Updated

--- src/Coilgun.cpp
+++ src/Coilgun.cpp
@@ -83,7 +83,6 @@ void Coilgun::kickOnceGotBall() {
 
 	std::cout << "! Kicking once got the ball" << std::endl;
 
-	com->send("nokick");
 	com->send("bdkick:1000:0:0:0");
 
 	isKickingOnceGotBall = true;
