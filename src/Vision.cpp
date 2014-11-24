2b868245b2728b0f165258770813d6fa5429e0b1
Priit Kallas <kallaspriit@gmail.com>
2014-11-25 01:11:30 +0200

    Updated

--- src/Vision.cpp
+++ src/Vision.cpp
@@ -920,13 +920,13 @@ Vision::PathMetric Vision::getPathMetric(int x1, int y1, int x2, int y2, std::ve
 	int step = originalX1 < originalX2 ? 1 : -1;
 	int sampleCount = 0;
 	float distanceStep = 0.05f;
-	//float distance1, distance2;
+	float distance1, distance2;
 
     for (int i = start; (originalX1 < originalX2 ? i < senseCounter : i >= 0); i += step) {
         x = senseX[i];
         y = senseY[i];
 
-		/*if (y > Config::cameraHeight / 4) {
+		if (y > Config::cameraHeight / 4) {
 			// sample less points near by and more in the distance
 			distance1 = getDistance(x, y).straight;
 			distance2 = Math::round(distance1 / distanceStep, 0) * distanceStep;
@@ -934,7 +934,7 @@ Vision::PathMetric Vision::getPathMetric(int x1, int y1, int x2, int y2, std::ve
 			if (Math::abs(distance1 - distance2) > distanceStep / 5.0f) {
 				continue;
 			}
-		}*/
+		}
 
 		sampleCount++;
 
