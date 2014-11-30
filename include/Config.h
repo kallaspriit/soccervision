c5ee8792906752bc2c7562eb131dd5e671aec7a9
Priit Kallas <kallaspriit@gmail.com>
2014-11-30 13:35:41 +0200

    Updated

--- include/Config.h
+++ include/Config.h
@@ -43,7 +43,7 @@ namespace Config {
 	//const float cameraFovAngle = 56.0f * 3.14f / 180.0f;
 	const float cameraFovAngle = 100.0f * 3.14f / 180.0f;
 	const float cameraFovWidth = tan(cameraFovAngle / 2.0f) * cameraFovDistance * 2.0f;
-	const int cameraGain = 2;
+	const int cameraGain = 4;
 	const int cameraExposure = 10000;
 
 	// default startup controller name
