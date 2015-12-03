93e37ad1db5a2514d8f17f7099c4e99539ac0a7b
Priit Kallas <kallaspriit@gmail.com>
2015-12-04 01:05:29 +0200

    Updated

--- src/Vision.cpp
+++ src/Vision.cpp
@@ -372,7 +372,7 @@ bool Vision::isValidGoal(Object* goal, Side side) {
 		edgeDistanceMetric.leftTopDistance.distance < Config::goalTopMinDistance
 		&& edgeDistanceMetric.rightTopDistance.distance < Config::goalTopMinDistance
 	) {
-		std::cout << "@ GOAL INVALID TOP EDGE DISTANCE LEFT: " << edgeDistanceMetric.leftTopDistance.distance << "m, right: " << edgeDistanceMetric.rightTopDistance.distance << "m" << std::endl;
+		// std::cout << "@ GOAL INVALID TOP EDGE DISTANCE LEFT: " << edgeDistanceMetric.leftTopDistance.distance << "m, right: " << edgeDistanceMetric.rightTopDistance.distance << "m" << std::endl;
 
 		return false;
 	}
